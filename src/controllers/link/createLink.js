d721c480c3e4a252e4e358d68ea7e22492f661e9
WeslyG <weslyg22@gmail.com>
2020-08-02 19:55:57 +0500

    Refactor createLink function for short it

--- src/controllers/link/createLink.js
+++ src/controllers/link/createLink.js
@@ -2,65 +2,57 @@ import { hash } from '../../utils/hash';
 import { LinkModel } from '../../models/linkModel';
 import { sendError } from '../../utils/sendError';
 import { HOSTNAME } from '../../../config';
-import { alreadyExistError } from '../../models/errorModel';
+import {
+  alreadyExistError,
+  notValidError,
+  linkRequired,
+  nameNotValid
+} from '../../dict/errorModel';
+
+const URL_REGEXP = /(^http(s)?:\/\/\w{2,})\.\w{2,}.*|(^(\w|\d){2,})\.\w{2,}.*/;
+
+export const checkNameCorrect = name => {
+  if (!name) return false;
+  name = name.toString();
+  if (name.match(/\d+/)) return false;
+  if (name.match(/^\w+$/)) return true;
+};
 
 export const checkNameFree = async name => {
   if (name === 'stats') return false;
-  if ((name.match(/^\w+$/))) {
-    const res = await LinkModel.find({
-      hash: name
-    });
-    return res.length === 0 ? true : false;
-  } else {
-    return false;
-  }
+  const res =  await LinkModel.find({
+    hash: name
+  });
+  return res.length === 0;
 };
 
-const saveLink = async (link, name) => {
+const saveLink = async body => {
   return await new LinkModel({
-    originalLink: link,
-    hash: name || hash(`${link}:${new Date().getTime()}`),
+    originalLink: body.link,
+    hash: body.name || hash(`${body.link}:${new Date().getTime()}`),
   }).save();
+};
+
+const returnLink = (res, link) => {
+  res.status(201).send({
+    originalLink: link.originalLink,
+    hash: link.hash,
+    shortLink: `${HOSTNAME}/${link.hash}`
+  });
 };
 
 export const createLink = async (req, res) => {
   try {
-    if (req.body.link) {
-      if (req.body.link.match(/(^http(s)?:\/\/\w{2,})\.\w{2,}.*|(^(\w|\d){2,})\.\w{2,}.*/) != null) {
-        if (req.body.name) {
-          const nameFree = await checkNameFree(req.body.name);
-          if (nameFree) {
-            const link = await saveLink(req.body.link, req.body.name);
-            res.status(201).send({
-              originalLink: link.originalLink,
-              hash: link.hash,
-              shortLink: `${HOSTNAME}/${link.hash}`
-            });
-            return;
-          } else {
-            sendError(res, alreadyExistError);
-          }
-        } else {
-          const link = await saveLink(req.body.link);
-          res.status(201).send({
-            originalLink: link.originalLink,
-            hash: link.hash,
-            shortLink: `${HOSTNAME}/${link.hash}`
-          });
-          return;
-        }
-      } else {
-        res.status(400).send({
-          message: 'link is not valid'
-        });
-        return;
-      }
-    } else res.status(400).send({
-      message: 'link is not valid http(s) link'
-    });
+    if (!req.body.link) return sendError(res, linkRequired);
+    if (!URL_REGEXP.test(req.body.link)) return sendError(res, notValidError);
+    if (req.body.name) {
+      if (!checkNameCorrect(req.body.name)) return sendError(res, nameNotValid);
+      if (!await checkNameFree(req.body.name)) return sendError(res, alreadyExistError);
+    }
+    return returnLink(res, await saveLink(req.body));
   } catch(err) {
     res.status(500).send({
-      message: 'error on create link'
+      message: 'error creating the link'
     });
   }
 };
