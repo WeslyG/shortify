8efddd498e2ae77f59cf9592fe6f9e2544fabf4b
WeslyG <weslyg22@gmail.com>
2020-08-02 19:51:57 +0500

    Refactor  SendError function

--- src/utils/sendError.js
+++ src/utils/sendError.js
@@ -1,13 +1,5 @@
 export const sendError = (res, err) => {
-  if (err.body) {
-    res.status(err.status).send(err.body);
-  } else if (err.message)
-    res.status(err.status).send({
-      message: err.message
-    });
-  else {
-    res.status(400).send({
-      message: 'Error model has error'
-    });
-  }
+  if (err.body) res.status(err.status).send(err.body);
+  else if (err.message) res.status(err.status).send({ message: err.message });
+  else res.status(400).send({ message: 'Error model has error' });
 };
